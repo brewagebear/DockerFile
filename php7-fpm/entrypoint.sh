35d7b242c9fd0bc15d1225966334494fc2d568bc
dailyworker <workingssu@gmail.com>
2019-04-25 16:54:42 +0900

    entrypoint.sh modified php5 to php7

--- php7-fpm/entrypoint.sh
+++ php7-fpm/entrypoint.sh
@@ -1,23 +1,26 @@
 #!/bin/sh
-PHP5_FPM_DEFAULT_SOCKET_TIMEOUT=${PHP5_FPM_DEFAULT_SOCKET_TIMEOUT:-"60"}
-PHP5_FPM_VALIDATE_TIMESTAMPS=${PHP5_FPM_VALIDATE_TIMESTAMPS:-"0"}
 
-sed -i -e "s/__PHP5_FPM_DEFAULT_SOCKET_TIMEOUT__/${PHP5_FPM_DEFAULT_SOCKET_TIMEOUT}/g" /etc/php/5.6/fpm/php.ini
-sed -i -e "s/__PHP5_FPM_VALIDATE_TIMESTAMPS__/${PHP5_FPM_VALIDATE_TIMESTAMPS}/g" /etc/php/5.6/fpm/php.ini
+PHP7_FPM_DEFAULT_SOCKET_TIMEOUT=${PHP7_FPM_DEFAULT_SOCKET_TIMEOUT:-"60"}
+PHP7_FPM_VALIDATE_TIMESTAMPS=${PHP7_FPM_VALIDATE_TIMESTAMPS:-"0"}
 
-if [ ! -z "$ENABLE_PHP5_FPM_V8" ]; then
-	if [ ! -f /etc/php/5.6/fpm/conf.d/20-v8js.ini ]; then
-		cd /etc/php/5.6/fpm/conf.d
-		ln -s /etc/php/5.6/mods-available/v8js.ini 20-v8js.ini
+sed -i -e "s/__PHP7_FPM_DEFAULT_SOCKET_TIMEOUT__/${PHP7_FPM_DEFAULT_SOCKET_TIMEOUT}/g" /etc/php/7.0/fpm/php.ini
+sed -i -e "s/__PHP7_FPM_VALIDATE_TIMESTAMPS__/${PHP7_FPM_VALIDATE_TIMESTAMPS}/g" /etc/php/7.0/fpm/php.ini
+
+if [ ! -z "$ENABLE_PHP7_FPM_V8" ]; then
+	if [ ! -f /etc/php/7.0/fpm/conf.d/20-v8js.ini ]; then
+		cd /etc/php/7.0/fpm/conf.d
+		ln -s /etc/php/7.0/mods-available/v8js.ini 20-v8js.ini
 	fi
 fi
 
-if [ ! -z "$PHP5_FPM_XDEBUG_REMOTE_HOST" ]; then
-	if [ ! -f /etc/php/5.6/fpm/conf.d/20-xdebug.ini ]; then
-		cd /etc/php/5.6/fpm/conf.d
-		ln -s /etc/php/5.6/mods-available/xdebug.ini 20-xdebug.ini
-		sed -i -e "s/__PHP5_FPM_XDEBUG_REMOTE_HOST__/${PHP5_FPM_XDEBUG_REMOTE_HOST}/g" /etc/php5/fpm/conf.d/20-xdebug.ini
+if [ ! -z "$PHP7_FPM_XDEBUG_REMOTE_HOST" ]; then
+	if [ ! -f /etc/php/7.0/fpm/conf.d/20-xdebug.ini ]; then
+		cd /etc/php/7.0/fpm/conf.d
+		ln -s /etc/php/7.0/mods-available/xdebug.ini 20-xdebug.ini
+		sed -i -e "s/__PHP7_FPM_XDEBUG_REMOTE_HOST__/${PHP7_FPM_XDEBUG_REMOTE_HOST}/g" /etc/php/7.0/fpm/conf.d/20-xdebug.ini
 	fi
 fi
+
+mkdir -p /run/php
 
 exec "$@"
